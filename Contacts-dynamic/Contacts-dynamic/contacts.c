6d512b05b84731e305c46d3903b0b23db5bfc535
PomDetom <111182697+PomDetom@users.noreply.github.com>
2024-04-27 23:58:10 +0800

    通讯录 动态内存版

---
+++ Contacts-dynamic/Contacts-dynamic/contacts.c
@@ -0,0 +1,220 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "contacts.h"
+
+//初始化
+void init_contacts(contacts* con)
+{
+	
+	con->count = 0;
+	con->capacity = 3;
+}
+
+//打印
+void show_contacts(const contacts* con)
+{
+	int i = 0;
+
+	if (con->count == 0)
+	{
+		printf("暂无信息，请添加联系人。\n");
+	}
+	else
+	{
+		printf("%-3s\t%-10s\t%-3s\t%-5s\t%-12s\t%-20s\n", "序号", "姓名", "年龄", "性别", "电话", "地址");
+		for (i = 0; i < con->count; i++)
+		{
+			printf("%-3d\t%-10s\t%-3d\t%-5s\t%-12s\t%-20s\n",
+				i + 1,
+				con->data[i].name,
+				con->data[i].age,
+				con->data[i].sex,
+				con->data[i].tel,
+				con->data[i].address);
+		}
+	}
+}
+
+//添加
+void add_infor(contacts* con)
+{
+	if (con->count == max_data)
+	{
+		printf("通讯录已满\n");
+	}
+	else
+	{
+		printf("开始添加第%d个联系人\n", con->count + 1);
+
+		printf("请输入姓名：");
+		scanf("%s", con->data[con->count].name);
+		printf("请输入年龄：");
+		scanf("%d", &con->data[con->count].age);
+		printf("请输入性别：");
+		scanf("%s", con->data[con->count].sex);
+		printf("请输入电话：");
+		scanf("%s", con->data[con->count].tel);
+		printf("请输入地址：");
+		scanf("%s", con->data[con->count].address);
+		con->count++;
+		printf("添加成功！\n");
+	}
+}
+
+//查找姓名
+int find_infor(const char input_name[], const contacts* con)
+{
+	int i = 0;
+
+	for (i = 0; i < con->count; i++)
+	{
+		if (strcmp(input_name, con->data[i].name) == 0)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+//删除
+void del_infor(contacts* con)
+{
+	if (con->count == 0)
+	{
+		printf("暂无信息，请添加联系人。\n");
+	}
+	else
+	{
+		char input_name[max_name] = { 0 };
+		printf("请输入要删除联系人的姓名:");
+		scanf("%s", input_name);
+		int ret = find_infor(input_name, con);
+
+		if (ret == -1)
+		{
+			printf("联系人不存在。\n");
+		}
+		else
+		{
+			int i = 0;
+
+			con->count--;
+			for (i = ret; i < con->count; i++)
+			{
+				con->data[i] = con->data[i + 1];
+			}
+			printf("删除成功！\n");
+		}
+	}
+}
+
+//修改
+void modify_infor(contacts* con)
+{
+	if (con->count == 0)
+	{
+		printf("暂无信息，请添加联系人。\n");
+	}
+	else
+	{
+		char input_name[max_name] = { 0 };
+		printf("请输入要修改联系人的姓名:");
+		scanf("%s", input_name);
+		int ret = find_infor(input_name, con);
+
+		if (ret == -1)
+		{
+			printf("联系人不存在。\n");
+		}
+		else
+		{
+			printf("开始修改\n");
+			printf("请输入姓名：");
+			scanf("%s", con->data[ret].name);
+			printf("请输入年龄：");
+			scanf("%d", &con->data[ret].age);
+			printf("请输入性别：");
+			scanf("%s", con->data[ret].sex);
+			printf("请输入电话：");
+			scanf("%s", con->data[ret].tel);
+			printf("请输入地址：");
+			scanf("%s", con->data[ret].address);
+
+			printf("修改成功！\n");
+		}
+	}
+}
+
+//查找
+void search_contacts(contacts* con)
+{
+	if (con->count == 0)
+	{
+		printf("暂无信息，请添加联系人。\n");
+	}
+	else
+	{
+		char input_name[max_name] = { 0 };
+		printf("请输入要查找联系人的姓名:");
+		scanf("%s", input_name);
+		int ret = find_infor(input_name, con);
+
+		if (ret == -1)
+		{
+			printf("联系人不存在。\n");
+		}
+		else
+		{
+			printf("已找到，是第%d个联系人。\n", ret + 1);
+			printf("%-10s\t%-3s\t%-5s\t%-12s\t%-20s\n", "姓名", "年龄", "性别", "电话", "地址");
+			printf("%-10s\t%-3d\t%-5s\t%-12s\t%-20s\n",
+				con->data[ret].name,
+				con->data[ret].age,
+				con->data[ret].sex,
+				con->data[ret].tel,
+				con->data[ret].address);
+		}
+	}
+}
+
+//按名称排序
+int compar_name(const void* a, const void* b)
+{
+	return strcmp(((infor*)a)->name, ((infor*)b)->name);
+}
+
+//按年龄排序
+int compar_age(const void* a, const void* b)
+{
+	return (((infor*)a)->age - ((infor*)b)->age);
+}
+
+//排序
+void sort_contacts(contacts* con)
+{
+	if (con->count == 0)
+	{
+		printf("暂无信息，请添加联系人。\n");
+	}
+	else
+	{
+		int input = 0;
+		printf("1.姓名 2.年龄 0.不排序\n");
+		printf("请选择排序方法 :");
+		scanf("%d", &input);
+
+		switch (input)
+		{
+		case 1:
+			qsort(con->data, con->count, sizeof(infor), compar_name);
+			printf("排序完成！\n");
+			break;
+		case 2:
+			qsort(con->data, con->count, sizeof(infor), compar_age);
+			printf("排序完成！\n");
+			break;
+		case 0:
+			break;
+		}
+	}
+}
---
+++ Contacts-dynamic/Contacts-dynamic/contacts.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+enum INFOR
+{
+	max_name = 10,
+	max_sex = 5,
+	max_tel = 12,
+	max_address = 20,
+	max_data = 100
+};
+
+//个人信息	
+typedef struct infor
+{
+	char name[max_name];
+	int age;
+	char sex[max_sex];
+	char tel[max_tel];
+	char address[max_address];
+}infor;
+
+//通讯录
+typedef struct contacts
+{
+	infor* data;
+	int count;
+	int capacity;
+}contacts;
+
+//初始化通讯录
+void init_contacts(contacts* con);
+
+//打印通讯录
+void show_contacts(const contacts* con);
+
+//添加联系人
+void add_infor(contacts* con);
+
+//删除联系人
+void del_infor(contacts* con);
+
+//修改联系人
+void modify_infor(contacts* con);
+
+//查找联系人
+void search_contacts(contacts* con);
+
+//排序联系人
+void sort_contacts(contacts* con);
